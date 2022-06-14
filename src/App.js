0a128518a63111ce1a1ec900866ac5705e0a38c2
UmidaImerova <umidagaliamova@gmail.com>
2022-06-14 18:06:25 +0600

    feat: create login component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import LoginPage from './components/Login/LoginPage'
 
 function App() {
--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,14 +1,57 @@
 import React from 'react'
 import style from './style.module.scss'
 import calendar from './assets/Calendar.png'
+import plusIcon from './assets/plusIcon.svg'
 
 function LoginPage() {
   return (
     <>
-      <div className={style.left}>left</div>
+      <div className={style.left}>
+        <div className={style.leftContent}>
+          <h1>Добро пожаловать!</h1>
+          <div className={style.addPhoto}>
+            <button type="button">
+              <img src={plusIcon} alt="icon" />
+            </button>
+            <span> Добавьте фото профиля </span>
+          </div>
+          <div className={style.personalForm}>
+            <form>
+              <label htmlFor="name">
+                ФИО
+                <input type="text" value="John" disabled="true" />
+              </label>
+              <label htmlFor="department">
+                Отдел
+                <input type="text" value="John" disabled="true" />
+              </label>
+              <label htmlFor="jobTitle">
+                Должность
+                <input type="text" value="John" disabled="true" />
+              </label>
+              <label htmlFor="email">
+                Почта
+                <input type="email" value="John" disabled="true" />
+              </label>
+              <label htmlFor="password">
+                Пароль
+                <input type="password" />
+              </label>
+              <label htmlFor="passwordConfirm">
+                Потвердите пароль
+                <input type="password" />
+              </label>
+              <textarea placeholder="О себе..." />
+              <button type="submit">СОХРАНИТЬ</button>
+            </form>
+          </div>
+        </div>
+      </div>
       <div className={style.right}>
-        <h1>Calendar</h1>
-        <img src={calendar} alt="calendar" />
+        <div className={style.rightContent}>
+          <h1>Calendar</h1>
+          <img src={calendar} alt="calendar" />
+        </div>
       </div>
     </>
   )
