948f7d07a56c8538aa7a631165a552709c8e12e8
UmidaImerova <umidagaliamova@gmail.com>
2022-06-28 18:02:48 +0600

    feat: header for profile page

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Grid, Stack } from '@mui/material'
+import { Button, Avatar, Stack } from '@mui/material'
 import TextField from '@mui/material/TextField'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
@@ -7,9 +7,20 @@ import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker'
 import s from './profileStyle.module.scss'
 import Logo from '../../components/Logo/Logo'
 import Tags from '../../components/Tags/Tags'
+import PrevItem from './assets/prevItem_icon.svg'
+import NextItem from './assets/nextItem_icon.svg'
+import SearchIcon from './assets/search_icon.svg'
+import AlarmIcon from './assets/alarm_icon.svg'
+import CloseIcon from './assets/close_icon.svg'
 
 function Profile() {
   const [value, setValue] = useState(new Date())
+  const [hideInput, setHideInput] = useState(true)
+
+  /* unhide search input */
+  const toggleIcon = () => {
+    setHideInput(!hideInput)
+  }
 
   return (
     <div className={s.profileWrapper}>
@@ -46,7 +57,49 @@ function Profile() {
         </LocalizationProvider>
         <Tags />
       </div>
-      <div className={s.header}>Header</div>
+      <div className={s.header}>
+        <div className={s.leftHeader}>
+          <button type="button" className={s.todayBtn}>
+            Сегодня
+          </button>
+          <select className={s.selectPeriod}>
+            <option value="day">День</option>
+            <option value="week">Неделя</option>
+            <option value="month">Месяц</option>
+          </select>
+          <div className={s.iconBtns}>
+            <button type="button" className={s.itemBtn}>
+              <img src={PrevItem} alt="previousItem" />
+            </button>
+            <button type="button" className={s.itemBtn}>
+              <img src={NextItem} alt="previousItem" />
+            </button>
+          </div>
+          <div className={s.selectedPeriod}>
+            <span>27 июня 2022 Monday</span>
+          </div>
+        </div>
+        <div className={s.rightHeader}>
+          <div className={s.searchInput}>
+            <label htmlFor="serchInput">
+              <input type={hideInput ? 'hidden' : 'text'} name="serach" id="serchInput" />
+              <button type="button" onClick={toggleIcon}>
+                <img src={hideInput ? SearchIcon : CloseIcon} alt="search_icon" />
+              </button>
+            </label>
+          </div>
+          <div className={s.notification}>
+            <button type="button">
+              <img src={AlarmIcon} alt="alarm_icon" />
+            </button>
+          </div>
+          <Avatar
+            sx={{ width: 32, height: 32, display: 'inline-flex' }}
+            alt="Remy Sharp"
+            src="/broken-image.jpg"
+          />
+        </div>
+      </div>
       <div className={s.content}>Content</div>
     </div>
   )
