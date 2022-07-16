c2c8fa576a68c95a586d082d66c7213ccf2f008a
UmidaImerova <umidagaliamova@gmail.com>
2022-07-16 14:56:14 +0600

    feat: month layout

--- src/components/Calendar/Month/MonthSheet.jsx
+++ src/components/Calendar/Month/MonthSheet.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import moment from 'moment'
+import 'moment/min/locales'
+import s from './monthStyle.module.scss'
 
 function MonthSheet() {
-  return <div>MonthSheet</div>
+  /* localisation for labrary moment js */
+  moment.locale('ru')
+  /* start of calenndar */
+  const startDay = moment().startOf('month').startOf('week')
+  /* end of calendar */
+  const endDay = moment().endOf('month').endOf('week')
+  /* create calendar */
+  /* шапка по дням недели */
+  const weekDays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+
+  /* В сетке месяца может быть максимально 6 недель по 7 дней = 42 элемента */
+  const totalDay = 42
+  const daysArray = [...Array(42)]
+
+  const calendar = []
+  const day = startDay.clone()
+  while (!day.isAfter(endDay)) {
+    calendar.push(day.clone())
+    day.add(1, 'day')
+  }
+  window.moment = moment()
+  window.startDay = startDay
+  window.endDay = endDay
+
+  return (
+    <div className={s.month}>
+      <div className={s.tableHeader}>
+        {weekDays.map((day) => (
+          <>
+            <div className={s.headerItem}>{day}</div>
+          </>
+        ))}
+      </div>
+      <div className={s.tableContent}>
+        {daysArray.map((_, i) => (
+          <div className={s.day}>{i}</div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default MonthSheet
